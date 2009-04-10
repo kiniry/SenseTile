9f5de20704d9451652962ebe54c902e498794b88
delbianc <delbianc@53849a8d-5926-474a-9f15-1b2e103828f6>
2009-04-10 18:05:08 +0000

    refactorings, finishing touches, documentation and some bug fixing to ByteMatcher

--- sb_driver/sb_driver/trunk/src/ie/ucd/sensetile/BytePattern.java
+++ sb_driver/sb_driver/trunk/src/ie/ucd/sensetile/BytePattern.java
@@ -1,89 +1,134 @@
+
 package ie.ucd.sensetile;
 
 import java.util.Arrays;
 
+/**
+ * Byte pattern matcher.
+ * 
+ * Find a pattern of bytes into a ByteArray.
+ * The pattern can be repeated.
+ * 
+ * @author delbianc
+ *
+ */
 public class BytePattern {
-    
-    private byte[] pattern;
-    private int repetitionStep;
-    
-    private BytePattern() {
+
+  private byte[] pattern;
+  private int repetitionStep;
+
+  private BytePattern() {
+  }
+
+  
+  /**
+   * Creation factory method.
+   * 
+   * Creates a pattern matcher based on pattern.
+   * 
+   * @param pattern to be matched. pattern cannot be null or empty.
+   * @return created pattern matcher.
+   */
+  static public BytePattern createPattern(byte[] pattern) {
+    checkPattern(pattern);
+    BytePattern creation = new BytePattern();
+    creation.pattern = Arrays.copyOf(pattern, pattern.length);
+    creation.repetitionStep = -1;
+    return creation;
+  }
+  
+  /**
+   * Creation factory method.
+   * 
+   * Creates a repetitive pattern matcher based on pattern, with a repetition
+   * of repetitionStep.
+   * 
+   * @param pattern to be matched. pattern cannot be null or empty.
+   * @param repetitionStep.
+   * @return created pattern matcher.
+   */
+  public static BytePattern createPattern(byte[] pattern, int repetitionStep) {
+    checkPattern(pattern);
+    checkRepetitionStep(repetitionStep);
+    BytePattern creation = new BytePattern();
+    creation.pattern = Arrays.copyOf(pattern, pattern.length);
+    creation.repetitionStep = repetitionStep;
+    return creation;
+  }
+  
+  /**
+   * Find a match.
+   * 
+   * Finds whether data matches the pattern.
+   * 
+   * @param data to be searched for pattern.
+   * @return index of begin of pattern found in data, or -1 if pattern is not 
+   * found.
+   */
+  public int match(byte[] data) {
+    if (data.length == 0) {
+      return -1;
     }
-    
-    static public BytePattern createPattern(byte[] pattern) {
-	checkPattern(pattern);
-	BytePattern creation = new BytePattern();
-	creation.pattern = Arrays.copyOf(pattern, pattern.length);
-	creation.repetitionStep = -1;
-	return creation;
+    int repetitionStep = (isPatternRepeated() ? this.repetitionStep
+        : data.length);
+    int index = matchSingle(data, repetitionStep);
+    if (index != -1 && isPatternRepeated()) {
+      index = checkNextRepetitions(index, data);
     }
-    
-    public static BytePattern createPattern(
-	    byte[] pattern,
-	    int repetitionStep) {
-	checkPattern(pattern);
-	BytePattern creation = new BytePattern();
-	creation.pattern = Arrays.copyOf(pattern, pattern.length);
-	creation.repetitionStep = repetitionStep;
-	return creation;
+    return index;
+  }
+
+  private boolean isPatternRepeated() {
+    return (repetitionStep != -1);
+  }
+
+  private int matchSingle(byte[] data, int length) {
+    int match_count = 0;
+    int iRecursive;
+    for (int i = 0; i < (length + pattern.length); i++) {
+      iRecursive = i % length;
+      byte b = data[iRecursive];
+      if (b == pattern[match_count]) {
+        match_count++;
+        if (match_count == pattern.length) {
+          return (i - pattern.length + 1);
+        }
+      } else {
+        if (match_count > 0) {
+          match_count = 0;
+        }
+      }
     }
-    
-    public int match(byte[] data) {
-	int repetitionStep = 
-	    ( isPatternRepeated() ? this.repetitionStep : data.length );
-	int index = matchSingle(data, repetitionStep);
-	if ( index != -1 && isPatternRepeated() ){
-	    index = checkNextRepetitions(index, data);
-	}
-	return index;
+    return -1;
+  }
+
+  private int checkNextRepetitions(int start, byte[] data) {
+    int dataIndex = start + repetitionStep;
+    int patternIndex = 0;
+    while (dataIndex < data.length) {
+      if (pattern[patternIndex] != data[dataIndex]) {
+        return -1;
+      }
+      dataIndex++;
+      patternIndex++;
+      if (patternIndex >= pattern.length) {
+        dataIndex = (dataIndex - patternIndex) + repetitionStep;
+        patternIndex = 0;
+      }
     }
-    
-    private boolean isPatternRepeated(){
-	return ( repetitionStep != -1 );
+    return start;
+  }
+
+  private static void checkPattern(byte[] pattern) {
+    if (pattern == null || pattern.length == 0) {
+      throw new IllegalArgumentException();
     }
-    
-    private int matchSingle(byte[] data, int length) {
-	int match_count = 0;
-	for (int i = 0; i < length; i++) {
-	    byte b = data[i];
-	    if (b == pattern[match_count]) {
-		match_count++;
-		if (match_count == pattern.length){
-		    return (i - pattern.length + 1);
-		}
-	    } else {
-		if ( match_count > 0 ){
-		    i =- match_count;
-		    match_count = 0;
-		}
-	    }
-	}
-	return -1;
+  }
+
+  private static void checkRepetitionStep(int repetition) {
+    if (repetition <= 0) {
+      throw new IllegalArgumentException();
     }
-    
-    private int checkNextRepetitions(int index, byte[] data) {
-	byte[] pattern = this.pattern;
-	int repetitionStep = this.repetitionStep;
-	int dataIndex = index + repetitionStep;
-	int patternIndex = 0;
-	while (dataIndex < data.length){
-	    if (pattern[patternIndex] != data[dataIndex]){
-		    return -1;
-	    }
-	    dataIndex++;
-	    patternIndex++;
-	    if(patternIndex >= pattern.length){
-		dataIndex = (dataIndex - patternIndex) + repetitionStep; 
-		patternIndex = 0;
-	    }
-	}
-	return index;
-    }
-    
-    private static void checkPattern(byte[] pattern) {
-	if (pattern == null || pattern.length == 0){
-	    throw new IllegalArgumentException();
-	}
-    }
-    
+  }
+
 }
--- sb_driver/sb_driver/trunk/test/src/ie/ucd/sensetile/BytePatternTest.java
+++ sb_driver/sb_driver/trunk/test/src/ie/ucd/sensetile/BytePatternTest.java
@@ -8,118 +8,176 @@ import org.junit.Test;
 
 public class BytePatternTest {
 
-    @Before
-    public void setUp() throws Exception {
-    }
+  @Before
+  public void setUp() throws Exception {
+  }
 
-    @After
-    public void tearDown() throws Exception {
-    }
+  @After
+  public void tearDown() throws Exception {
+  }
 
-    @Test
-    public void testCreatePattern() {
-        byte[] dataPattern = {3,4,5,6};
-        BytePattern pattern = BytePattern.createPattern(dataPattern);
-        assertNotNull(pattern);
-    }
-    
-    @Test(expected=IllegalArgumentException.class) 
-    public void testCreatePatternEmptyPattern() {
-        byte[] dataPattern = {};
-        BytePattern.createPattern(dataPattern);
-    }
-    
-    @Test(expected=IllegalArgumentException.class) 
-    public void testCreatePatternNullPattern() {
-        byte[] dataPattern = null;
-        BytePattern.createPattern(dataPattern);
-    }
-    
-    @Test
-    public void testIdentifyPattern(){
-        byte[] dataPattern = {3,4,5,6};
-        BytePattern pattern = BytePattern.createPattern(dataPattern);
-        byte[] data = {0,1,2,0,4,5,6,7,8,9,0,1,2,3,4,5,6,7,8,9,0};
-        assertEquals(13, pattern.match(data));
-    }
-    
-    @Test
-    public void testMatchSingleBytePattern(){
-        byte[] dataPattern = {6};
-        BytePattern pattern = BytePattern.createPattern(dataPattern);
-        byte[] data = {0,1,2,3,4,5,6,7,8,9,0,1,2,3,4,5,6,7,8,9,0};
-        assertEquals(6, pattern.match(data));
-    }
-    
-    @Test
-    public void testMatchEmptyPattern(){
-        byte[] dataPattern = {6};
-        BytePattern pattern = BytePattern.createPattern(dataPattern);
-        byte[] data = {0,1,2,3,4,5,6,7,8,9,0,1,2,3,4,5,6,7,8,9,0};
-        assertEquals(6, pattern.match(data));
-    }
-    
-    @Test
-    public void testMatchNotFoundPattern(){
-        byte[] dataPattern = {10};
-        BytePattern pattern = BytePattern.createPattern(dataPattern);
-        byte[] data = {0,1,2,3,4,5,6,7,8,9,0,1,2,3,4,5,6,7,8,9,0};
-        assertEquals(-1, pattern.match(data));
-    }
+  @Test
+  public void testCreate() {
+    byte[] dataPattern = { 3, 4, 5, 6 };
+    BytePattern pattern = BytePattern.createPattern(dataPattern);
+    assertNotNull(pattern);
+  }
 
-    @Test
-    public void testMatchSmallData(){
-        byte[] dataPattern = {1,2,3};
-        BytePattern pattern = BytePattern.createPattern(dataPattern);
-        byte[] data = {0};
-        assertEquals(-1, pattern.match(data));
-    }
-    
-    @Test
-    public void testMatchEmptyData(){
-        byte[] dataPattern = {1,2,3};
-        BytePattern pattern = BytePattern.createPattern(dataPattern);
-        byte[] data = {};
-        assertEquals(-1, pattern.match(data));
-    }
-    
-    @Test
-    public void testCreatePatternRepeated(){
-        byte[] dataPattern = {3,4};
-        int repetitionStep = 5;
-        BytePattern pattern = 
-            BytePattern.createPattern(dataPattern, repetitionStep);
-        assertNotNull(pattern);
-    }
-    
-    @Test
-    public void testMatchPatternRepeated(){
-        byte[] dataPattern = {3,4};
-        int repetitionStep = 5;
-        BytePattern pattern = 
-            BytePattern.createPattern(dataPattern, repetitionStep);
-        byte[] data = {0,1,2,3,4,0,1,2,3,4,0,1,2,3,4, 0};
-        assertEquals(3, pattern.match(data));
-    }
-    
-    @Test
-    public void testMatchPatternRepeatedTruncated(){
-        byte[] dataPattern = {3,4};
-        int repetitionStep = 5;
-        BytePattern pattern = 
-            BytePattern.createPattern(dataPattern, repetitionStep);
-        byte[] data = {0,1,2,3,4,0,1,2,3,4,0,1,2,3};
-        assertEquals(3, pattern.match(data));
-    }
-    
-    @Test
-    public void testMatchPatternRepeatedNotFound(){
-        byte[] dataPattern = {3,4};
-        int repetitionStep = 5;
-        BytePattern pattern = 
-            BytePattern.createPattern(dataPattern, repetitionStep);
-        byte[] data = {0,1,2,3,4,0,1,2,3,4,0,1,2,3,3};
-        assertEquals(-1, pattern.match(data));
-    }
-    
+  @Test(expected = IllegalArgumentException.class)
+  public void testCreateEmpty() {
+    byte[] dataPattern = {};
+    BytePattern.createPattern(dataPattern);
+  }
+
+  @Test(expected = IllegalArgumentException.class)
+  public void testCreateNull() {
+    byte[] dataPattern = null;
+    BytePattern.createPattern(dataPattern);
+  }
+
+  @Test
+  public void testMatch() {
+    byte[] dataPattern = { 3, 4, 5, 6 };
+    BytePattern pattern = BytePattern.createPattern(dataPattern);
+    byte[] data = { 0, 1, 2, 0, 4, 5, 6, 7, 8, 9, 0, 1, 2, 3, 4, 5, 6, 7, 8,
+        9, 0 };
+    assertEquals(13, pattern.match(data));
+  }
+
+  @Test
+  public void testMatchOnEdge() {
+    byte[] dataPattern = { 3, 4 };
+    BytePattern pattern = BytePattern.createPattern(dataPattern);
+    byte[] leftEdge = { 3, 4, 2, 0, 4, 5, 6, 7, 8, 9, 0, 1, 2, 0, 4, 5, 6, 7,
+        8, 9, 0 };
+    assertEquals(0, pattern.match(leftEdge));
+    byte[] rightEdge = { 0, 1, 2, 0, 4, 5, 6, 7, 8, 3, 4 };
+    assertEquals(9, pattern.match(rightEdge));
+  }
+
+  @Test
+  public void testMatchRecursive() {
+    byte[] dataPattern = { 3, 4, 5, 6 };
+    BytePattern pattern = BytePattern.createPattern(dataPattern);
+    byte[] data = {5, 6, 0, 0, 3, 4 };
+    assertEquals(4, pattern.match(data));
+  }
+
+  @Test
+  public void testMatchSingleByte() {
+    byte[] dataPattern = { 6 };
+    BytePattern pattern = BytePattern.createPattern(dataPattern);
+    byte[] data = { 0, 1, 2, 3, 4, 5, 6, 7, 6, 9};
+    assertEquals(6, pattern.match(data));
+  }
+
+  @Test
+  public void testMatchNotFound() {
+    byte[] dataPattern = { 8, 9, 10 };
+    BytePattern pattern = BytePattern.createPattern(dataPattern);
+    byte[] data = { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 0, 1, 2};
+    assertEquals(-1, pattern.match(data));
+  }
+
+  @Test
+  public void testMatchSingleData() {
+    byte[] dataPattern = { 1, 2, 3 };
+    BytePattern pattern = BytePattern.createPattern(dataPattern);
+    byte[] data = { 0 };
+    assertEquals(-1, pattern.match(data));
+  }
+
+  @Test
+  public void testMatchSingleByteSingleData() {
+    byte[] dataPattern = { 6 };
+    BytePattern pattern = BytePattern.createPattern(dataPattern);
+    byte[] data = { 6};
+    assertEquals(0, pattern.match(data));
+  }
+
+  @Test
+  public void testMatchEmptyData() {
+    byte[] dataPattern = { 1, 2, 3 };
+    BytePattern pattern = BytePattern.createPattern(dataPattern);
+    byte[] data = {};
+    assertEquals(-1, pattern.match(data));
+  }
+
+  @Test
+  public void testCreateRepeated() {
+    byte[] dataPattern = { 3, 4 };
+    int repetitionStep = 5;
+    BytePattern pattern = BytePattern.createPattern(
+        dataPattern, repetitionStep);
+    assertNotNull(pattern);
+  }
+
+  @Test(expected = IllegalArgumentException.class)
+  public void testCreateRepeated0Repetition() {
+    byte[] dataPattern = { 3, 4 };
+    int repetitionStep = 0;
+    BytePattern pattern = BytePattern.createPattern(
+        dataPattern, repetitionStep);
+    assertNotNull(pattern);
+  }
+
+  @Test(expected = IllegalArgumentException.class)
+  public void testCreateRepeatedNegativeRepetition() {
+    byte[] dataPattern = { 3, 4 };
+    int repetitionStep = -4;
+    BytePattern pattern = BytePattern.createPattern(
+        dataPattern, repetitionStep);
+    assertNotNull(pattern);
+  }
+
+  @Test
+  public void testMatchRepeated() {
+    byte[] dataPattern = { 3, 4 };
+    int repetitionStep = 5;
+    BytePattern pattern = BytePattern.createPattern(
+        dataPattern, repetitionStep);
+    byte[] data = { 0, 1, 2, 3, 4, 0, 1, 2, 3, 4, 0, 1, 2, 3, 4, 0 };
+    assertEquals(3, pattern.match(data));
+  }
+  
+  @Test
+  public void testMatchRepeatedSingleRepetition() {
+    byte[] dataPattern = { 3, 4 };
+    int repetitionStep = 5;
+    BytePattern pattern = BytePattern.createPattern(
+        dataPattern, repetitionStep);
+    byte[] data = { 0, 1, 2, 3, 4, 0, 1, 2};
+    assertEquals(3, pattern.match(data));
+  }
+
+  @Test
+  public void testMatchRepeatedTruncated() {
+    byte[] dataPattern = { 3, 4 };
+    int repetitionStep = 5;
+    BytePattern pattern = BytePattern.createPattern(
+        dataPattern, repetitionStep);
+    byte[] data = { 0, 1, 2, 3, 4, 0, 1, 2, 3, 4, 0, 1, 2, 3 };
+    assertEquals(3, pattern.match(data));
+  }
+
+  @Test
+  public void testMatchRepeatedRecursiveTruncated() {
+    byte[] dataPattern = { 4, 0 };
+    int repetitionStep = 5;
+    BytePattern pattern = BytePattern.createPattern(
+        dataPattern, repetitionStep);
+    byte[] data = { 0, 1, 2, 3, 4, 0, 1, 2, 3, 4, 0, 1, 2, 3, 4 };
+    assertEquals(4, pattern.match(data));
+  }
+
+  @Test
+  public void testMatchRepeatedNotFound() {
+    byte[] dataPattern = { 3, 4 };
+    int repetitionStep = 5;
+    BytePattern pattern = BytePattern.createPattern(
+        dataPattern, repetitionStep);
+    byte[] data = { 0, 1, 2, 3, 4, 0, 1, 2, 3, 4, 0, 1, 2, 3, 3 };
+    assertEquals(-1, pattern.match(data));
+  }
+
 }
