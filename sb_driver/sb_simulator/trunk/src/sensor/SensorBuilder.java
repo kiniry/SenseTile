c9c73b0e6f96c404e6292210cccb4bebccdf94c5
dragan <dragan@53849a8d-5926-474a-9f15-1b2e103828f6>
2009-08-07 11:42:20 +0000

    first version of SensorBuilder

---
+++ sb_driver/sb_simulator/trunk/src/sensor/SensorBuilder.java
@@ -0,0 +1,239 @@
+package sensor;
+
+
+import java.util.ArrayList;
+import java.util.List;
+
+import channel.ChannelException;
+import channel.FileChannel;
+import channel.IChannel;
+
+import sensor.type.SensorType;
+ 
+/**
+ * Represents an sensor builder implementation. 
+ * @author dragan
+ */
+public final class SensorBuilder 
+{
+	private final transient /*@ spec_public non_null@*/ List sensors;
+	private static final String THERM_FILE_PATH = "";
+	private static final String PRESS_FILE_PATH = "";
+	private static final String AUDIO_FILE_PATH = "";
+	private static final String ULTRA_FILE_PATH = "";
+	private static final String LIGHT_FILE_PATH = "";
+	private static final String ACCEL_X_FILE_PATH = "";
+	private static final String ACCEL_Y_FILE_PATH = "";
+	private static final String ACCEL_Z_FILE_PATH = "";
+	
+		
+	//@ public model int mod_size;
+	//@ represents mod_size \such_that mod_size == sensors.size();
+	
+	//@ invariant sensors.elementType == \type(ISensor);
+	//@ invariant !sensors.containsNull;
+	
+	//@ constraint mod_size <= 8;
+	
+	//@ assignable sensors;
+	public SensorBuilder()
+	{
+		 sensors = new ArrayList();
+		//@ set sensors.elementType = \type(ISensor);
+		//@ set sensors.containsNull = false;
+	}
+	
+	//@ requires mod_size == 0;
+	//@ assignable \not_specified;
+	//@ ensures mod_size == 1;
+	 public void initialize()
+	 {
+	 	createThermistorSensor();
+		createLightSensor();
+		createSoundSensor();
+		createPressureSensor();
+		createUltrasonicSensor();
+		createAxesXSensor();
+		createAxesYSensor();
+		createAxesZSensor();
+	  }
+	
+	/*@ requires mod_size == 0;
+	  @ assignable \not_specified;
+	  @ ensures mod_size == \old(mod_size) +1;
+	  @*/
+	private void createThermistorSensor()
+	{
+		final IChannel therm_channel = new FileChannel(THERM_FILE_PATH);
+		ISensor sensor;
+		try
+		{
+			final int[] arr = therm_channel.getArray();
+			
+			sensor = new ThermistorSensor( arr );
+		}
+		catch (ChannelException cex)
+		{
+			final int[] arr = new int[]{};
+			sensor = new ThermistorSensor( arr );
+			sensor.setEnable(false);
+		}
+		sensors.add( sensor );
+	}
+	
+	//@ requires mod_size == 1;
+	//@ assignable \not_specified;
+	//@ ensures mod_size == \old(mod_size) +1;
+	private void createLightSensor()
+	{
+		final IChannel light_channel = new FileChannel(LIGHT_FILE_PATH);
+		ISensor sensor;
+		try
+		{
+			final int[] arr = light_channel.getArray();
+			sensor = new LightSensor( arr );
+		}
+		catch (ChannelException cex)
+		{
+			final int[] arr = new int[]{};
+			sensor = new LightSensor( arr );
+			sensor.setEnable(false);
+		}
+		sensors.add( sensor );
+	}
+	//@ requires mod_size == 2;
+	//@ assignable \not_specified;
+	//@ ensures mod_size == \old(mod_size) +1;
+	private void createSoundSensor()
+	{
+		final IChannel sound_channel = new FileChannel(AUDIO_FILE_PATH);
+		ISensor sensor;
+		try
+		{
+			final int[] arr = sound_channel.getArray();
+			sensor = new SoundSensor( arr );
+		}
+		catch (ChannelException cex)
+		{
+			final int[] arr = new int[]{};
+			sensor = new SoundSensor( arr );
+			sensor.setEnable(false);
+		}
+		sensors.add( sensor );
+	}
+	//@ requires mod_size == 3;
+	//@ assignable \not_specified;
+	//@ ensures mod_size == \old(mod_size) +1;
+	private void createPressureSensor()
+	{
+		final IChannel press_channel = new FileChannel(PRESS_FILE_PATH);
+		ISensor sensor;
+		try
+		{
+			final int[] arr = press_channel.getArray();
+			sensor = new PressureSensor( arr );
+		}
+		catch (ChannelException cex)
+		{
+			final int[] arr = new int[]{};
+			sensor = new PressureSensor( arr );
+			sensor.setEnable(false);
+		}
+		sensors.add( sensor );
+	}
+	
+	//@ requires mod_size == 4;
+	//@ assignable \not_specified;
+	//@ ensures mod_size == \old(mod_size) +1;
+	private void createUltrasonicSensor()
+	{
+		final IChannel ultra_channel = new FileChannel(ULTRA_FILE_PATH);
+		ISensor sensor;
+		try
+		{
+			final int[] arr = ultra_channel.getArray();
+			sensor = new UltrasonicSensor( arr );
+		}
+		catch (ChannelException cex)
+		{
+			final int[] arr = new int[]{};
+			sensor = new UltrasonicSensor( arr );
+			sensor.setEnable(false);
+		}
+		sensors.add( sensor );
+	}
+	
+	//@ requires mod_size == 5;
+	//@ assignable \not_specified;
+	//@ ensures mod_size == \old(mod_size) +1;
+	private void createAxesXSensor()
+	{
+		final IChannel accel_channel = new FileChannel(ACCEL_X_FILE_PATH);
+		ISensor sensor;
+		try
+		{
+			final int[] arr = accel_channel.getArray();
+			sensor = new AxisAccelerometerSensor( arr , SensorType.ACCEL_X );
+		}
+		catch (ChannelException cex)
+		{
+			final int[] arr = new int[]{};
+			sensor = new AxisAccelerometerSensor( arr , SensorType.ACCEL_X );
+			sensor.setEnable(false);
+		}
+		sensors.add( sensor );
+	}
+	
+	//@ requires mod_size == 6;
+	//@ assignable \not_specified;
+	//@ ensures mod_size == \old(mod_size) +1;
+	private void createAxesYSensor()
+	{
+		IChannel accel_channel = new FileChannel(ACCEL_Y_FILE_PATH);
+		ISensor sensor;
+		try
+		{
+			final int[] arr = accel_channel.getArray();
+			sensor = new AxisAccelerometerSensor( arr , SensorType.ACCEL_Y );
+		}
+		catch (ChannelException cex)
+		{
+			final int[] arr = new int[]{};
+			sensor = new AxisAccelerometerSensor( arr , SensorType.ACCEL_Y );
+			sensor.setEnable(false);
+		}
+		sensors.add( sensor );
+	}
+	
+	//@ requires mod_size == 7;
+	//@ assignable \not_specified;
+	//@ ensures mod_size == \old(mod_size) +1;
+	private void createAxesZSensor()
+	{
+		IChannel accel_channel = new FileChannel(ACCEL_Z_FILE_PATH);
+		ISensor sensor;
+		try
+		{
+			final int[] arr = accel_channel.getArray();
+			sensor = new AxisAccelerometerSensor( arr , SensorType.ACCEL_Z );
+		}
+		catch (ChannelException cex)
+		{
+			final int[] arr = new int[]{};
+			sensor = new AxisAccelerometerSensor( arr , SensorType.ACCEL_Z );
+			sensor.setEnable(false);
+		}
+		sensors.add( sensor );
+	}
+	
+	
+	//@ requires mod_size == 8;
+	//@ requires typeIndex >=0 && typeIndex <=7;
+	//@ requires SensorType.legal_SensorType(typeIndex);
+	//@ ensures \result instanceof ISensor;
+	public ISensor getSensor(final int typeIndex) 
+	{
+		return (ISensor)sensors.get(typeIndex);
+	}
+	  
+}
