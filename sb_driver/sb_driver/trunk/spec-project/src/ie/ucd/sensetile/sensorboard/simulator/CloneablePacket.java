b325ccc7895c466ed8dccf45ca8353f744b27faa
delbianc <delbianc@53849a8d-5926-474a-9f15-1b2e103828f6>
2009-08-24 16:55:16 +0000

    Sensor Board Driver Java: project refactorings

--- sb_driver/sb_driver/trunk/spec-project/src/ie/ucd/sensetile/sensorboard/simulator/CloneablePacket.java
+++
@@ -1,9 +0,0 @@
-package ie.ucd.sensetile.sensorboard.simulator;
-
-import ie.ucd.sensetile.sensorboard.Packet;
-
-public interface CloneablePacket extends Packet, Cloneable {
-
-  CloneablePacket clone() throws CloneNotSupportedException;
-
-}
