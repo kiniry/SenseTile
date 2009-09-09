d61d6ca2261d64c4ea4bfea44c395c02f2d5f0eb
delbianc <delbianc@53849a8d-5926-474a-9f15-1b2e103828f6>
2009-09-09 12:10:51 +0000

    Sensor Board Driver Java: cleanup

--- sb_driver/sb_driver/trunk/main-project/escj_driver.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-JAVA_HOME="/usr/local/lib/jvm/java-1.4.2-sun"
-CLASSPATH="/usr/local/KindSoftware/ESCJava/2.0.5/specs:spec:src"
-PATH="${JAVA_HOME}/bin:${PATH}"
-
-escj -Specs spec/lib/specs-java1.4:spec/src -ClassPath spec/lib:spec/src:src src/ie/ucd/sensetile/sensorboard/PacketInputStream.java
-escj -Specs spec/lib/specs-java1.4:spec/src -ClassPath spec/lib:spec/src:src src/ie/ucd/sensetile/sensorboard/SensorBoardPacket.java
