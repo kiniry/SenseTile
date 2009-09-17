cd634f4a33e33ffdf28206f5b80a58f79025dfb4
delbianc <delbianc@53849a8d-5926-474a-9f15-1b2e103828f6>
2009-09-17 12:33:47 +0000

    SenseTile Sensor Board driver: JUnit 3 compatibility on tests suites

--- sb_driver/sb_driver/trunk/test-project/src/ie/ucd/sensetile/AllUnitTests.java
+++ sb_driver/sb_driver/trunk/test-project/src/ie/ucd/sensetile/AllUnitTests.java
@@ -1,5 +1,8 @@
 package ie.ucd.sensetile;
 
+import junit.framework.JUnit4TestAdapter;
+import junit.framework.Test;
+
 import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
 
@@ -11,6 +14,10 @@ import org.junit.runners.Suite;
   ie.ucd.sensetile.util.AllUnitTests.class})
 
 public class AllUnitTests {
+  
+  public static Test suite() {
+    return new JUnit4TestAdapter(AllUnitTests.class);
+  }
   
   public static void main(final String[] args) {
     org.junit.runner.JUnitCore.main("ie.ucd.sensetile.AllUnitTests");
