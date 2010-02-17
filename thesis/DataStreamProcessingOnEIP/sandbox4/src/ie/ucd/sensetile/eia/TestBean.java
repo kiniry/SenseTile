fe88f3a526e94daa3d38de9e4d00ff8f43548c99
damianchojna <damianchojna@53849a8d-5926-474a-9f15-1b2e103828f6>
2010-02-17 14:46:39 +0000

    test commit

--- thesis/DataStreamProcessingOnEIP/sandbox4/src/ie/ucd/sensetile/eia/TestBean.java
+++ thesis/DataStreamProcessingOnEIP/sandbox4/src/ie/ucd/sensetile/eia/TestBean.java
@@ -3,17 +3,32 @@ package ie.ucd.sensetile.eia;
 import ie.ucd.sensetile.eia.data.CompositeDataPacket;
 
 import org.apache.camel.Exchange;
+import org.apache.camel.Processor;
 
-public class TestBean {
+public class TestBean implements Processor {
 
 	int counter = 0;
 	
+	String name = null;
+	
+	public TestBean(String id) {
+		this.name = id;
+	}
+	
 	public void process(Exchange e) {
-		if (counter % 100 == 0) {
-			CompositeDataPacket p  = (CompositeDataPacket)e.getIn().getBody();
-			System.out.println("TestBean.process():" + counter + " " + p.getPrimaryChannelData().length);
-		}
+		go2(e);
 		counter++;
 	}
 	
+	public void go1(Exchange e) {
+		CompositeDataPacket p  = (CompositeDataPacket)e.getIn().getBody();
+		System.out.println("TestBean." + name + ":"  + counter + " " + p.getPrimaryChannelData().length);	
+	}
+	
+	public void go2(Exchange e) {
+		if (counter % 100 == 0) {
+			CompositeDataPacket p  = (CompositeDataPacket)e.getIn().getBody();
+			System.out.println("TestBean." + name + ":"  + counter + " " + p.getPrimaryChannelData().length);
+		}	
+	}
 }
